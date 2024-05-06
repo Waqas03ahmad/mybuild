7e32424ce67aaf44b4e752df6f06dc6fb4accced
Waqas03ahmad <vickytrader03@gmail.com>
2024-05-06 00:22:55 -0700

    'longgg'

--- src/pages/Dashboard/Teacher.jsx
+++ src/pages/Dashboard/Teacher.jsx
@@ -1,39 +1,85 @@
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import { Box, Link, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 export default function Teacher() {
   return (
-    <List sx={{ width: '100%', bgcolor: 'background.paper', }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
-    </List>
+    <>
+      <Box
+        sx={{
+          marginX: 1,
+          marginTop: 1,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography color="initial">Top Teachers</Typography>
+        <Link component={NavLink} to={"/professors"} sx={{ cursor: "pointer" }}>
+          view all
+        </Link>
+      </Box>
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        {[
+          {
+            name: "Sajjad Ahmad",
+            position: "Lecturer",
+            department: "BSCS",
+            photo: "../../../public/sajjad.png",
+          },
+          {
+            name: "Sajjad Ahmad",
+            position: "Lecturer",
+            department: "BSCS",
+            photo: "../../../public/sajjad.png",
+          },
+          {
+            name: "Sajjad Ahmad",
+            position: "Lecturer",
+            department: "BSCS",
+            photo: "../../../public/sajjad.png",
+          },
+          {
+            name: "Sajjad Ahmad",
+            position: "Lecturer",
+            department: "BSCS",
+            photo: "../../../public/sajjad.png",
+          },
+          {
+            name: "Sajjad Ahmad",
+            position: "Lecturer",
+            department: "BSCS",
+            photo: "../../../public/sajjad.png",
+          },
+        ].map((e) => (
+          <ListItem key={e} sx={{ margin: 1, padding: 0 }}>
+            <ListItemAvatar>
+              <Avatar>
+                <img
+                  src={e.photo}
+                  alt="sajjad"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={e.name}
+              secondary={`${e.position} ${e.department}`}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
+[
+  {
+    name: "Sajjad Ahmad",
+    position: "Lecturer",
+    department: "BSCS",
+    photo: "../../../public/sajjad.png",
+  },
+];
